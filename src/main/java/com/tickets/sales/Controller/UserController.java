e6801f6106216c09adca643fd0c6034074e09b57
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-12-03 20:14:10 -0500

    Registro/Login operativo

--- src/main/java/com/tickets/sales/Controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.tickets.sales.Controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-
-
-}
--- src/main/java/com/tickets/sales/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tickets.sales.Repository;
-
-import com.tickets.sales.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
