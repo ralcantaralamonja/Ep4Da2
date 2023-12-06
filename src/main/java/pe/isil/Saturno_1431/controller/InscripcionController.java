f6de8db683132ccd4ff31402c23de7b7ac0c4d5d
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-12-05 21:23:17 -0500

    intentando consola logger

--- src/main/java/pe/isil/Saturno_1431/controller/InscripcionController.java
+++ src/main/java/pe/isil/Saturno_1431/controller/InscripcionController.java
@@ -1,22 +1,37 @@
 package pe.isil.Saturno_1431.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pe.isil.Saturno_1431.model.Inscripcion;
+
+import java.util.logging.Logger;
 
 @Controller
 public class InscripcionController {
 
-    @PostMapping
-    public String procesarInscripcion(@ModelAttribute("inscripcion") Inscripcion inscripcion) {
-        int totalEntradas = inscripcion.getNinos()  + inscripcion.getAdultos() + inscripcion.getAdultosMayor() + inscripcion.getDeportistasCalificados();
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(InscripcionController.class);
 
-        // Aquí puedes hacer lo que necesites con la información de las entradas,
-        // como calcular el precio total, aplicar descuentos, guardar en la base de datos, etc.
+    @PostMapping("/calcularDescuento")
+    public String procesarEntradas(@RequestParam("ninos") int cantidadNinos,
+                                   @RequestParam("adultos") int cantidadAdultos,
+                                   @RequestParam("adultosMayor") int cantidadAdultosMayor,
+                                   @RequestParam("deportistasCalificados") int cantidadDeportistasCalificados) {
+        // Realizar las operaciones necesarias con la cantidad de entradas recibidas
 
-        // Redirige a donde necesites después de procesar la inscripción
-        return "redirect:/ruta-donde-quieras-redirigir";
+        int totalEntradas = cantidadNinos + cantidadAdultos + cantidadAdultosMayor + cantidadDeportistasCalificados;
+
+        System.out.println("Cantidad de entradas:");
+        System.out.println("Total: " + totalEntradas);
+        System.out.println("Niños: " + cantidadNinos);
+        System.out.println("Adultos: " + cantidadAdultos);
+        System.out.println("Adultos Mayor: " + cantidadAdultosMayor);
+        System.out.println("Deportistas Calificados: " + cantidadDeportistasCalificados);
+        // Realizar otras operaciones con los datos recibidos
+
+        // Redirigir a donde desees después de procesar las entradas
+        return "index";
     }
-
 }
